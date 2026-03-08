ab2e121ccfe2905f04931f8beef3099830edb7fc
AlMonther Abdulhafeez <ealmonzer667@gmail.com>
2026-03-09 01:31:16 +0200

    style: add extra padding top to the footer to separate the last section of the page from the footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -51,7 +51,7 @@ const socialLinks: SocialLink[] = [
 
 export default function Footer() {
     return (
-        <footer className="footer-root relative w-full px-5 sm:px-8 md:px-10 lg:px-20 pt-6 sm:pt-8 pb-4 sm:pb-6 flex flex-col bg-gradient-secondary text-white overflow-hidden">
+        <footer className="footer-root relative w-full px-5 sm:px-8 md:px-10 lg:px-20 pt-6 sm:pt-16 pb-4 sm:pb-6 flex flex-col bg-gradient-secondary text-white overflow-hidden">
             {/* Upper content */}
             <div className="relative z-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-12">
                 <nav aria-label="Footer Quick Links">
